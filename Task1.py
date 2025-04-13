f30991ef7928565c42c4d3c1795ccf00e4a0a36d
SameGevDudios <magomed.chapaev1@mail.ru>
2025-04-13 20:25:42 +0300

    Add deriving Shape classes

--- Task1.py
+++ Task1.py
@@ -18,3 +18,19 @@ class Shape:
         y = [p[1] for p in self.points]
         return abs(sum(x[i] * y[(i+1)%len(self.points)] - x[(i+1)%len(self.points)] * y[i]
                        for i in range(len(self.points))) / 2)
+
+
+class Pentagon(Shape):
+    def __init__(self, identifier: str, points: list[tuple[float, float]]):
+        if len(points) != 5:
+            raise InvalidShapeError("Пятиугольник должен иметь 5 точек")
+        super().__init__(identifier, points)
+
+
+class Triangle(Shape):
+    def __init__(self, identifier: str, points: list[tuple[float, float]]):
+        if len(points) != 3:
+            raise InvalidShapeError("Треугольник должен иметь 3 точки")
+        super().__init__(identifier, points)
+
+
